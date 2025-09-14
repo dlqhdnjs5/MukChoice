d66997e0eee251cd9a8dd99b2100a9c74e8822e3
이보원 <bowon.lee@nts-corp.com>
2025-09-14 12:04:46 +0900

    confilct 수정

--- src/main/kotlin/com/project/mukchoice/config/WebMvcConfig.kt
+++ src/main/kotlin/com/project/mukchoice/config/WebMvcConfig.kt
@@ -5,10 +5,7 @@ import com.project.mukchoice.manager.JwtManager
 import com.project.mukchoice.service.UserService
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
 
 @Configuration
 class WebMvcConfig(
@@ -17,22 +14,6 @@ class WebMvcConfig(
 ) : WebMvcConfigurer {
     companion object {
         private const val PATTERN_ALL = "/**"
-    }
-
-    override fun addViewControllers(registry: ViewControllerRegistry) {
-        // 1단계(/abc)
-        registry.addViewController("/{path:^(?!api|oauth|actuator|.*\\..*$).*$}")
-            .setViewName("forward:/index.html")
-        // 2단계 이상(/abc/def, /kakao-oauth/redirect 등)
-        registry.addViewController("/**/{path:^(?!api|oauth|actuator|.*\\..*$).*$}")
-            .setViewName("forward:/index.html")
-    }
-
-    // 정적 리소스 핸들러 (통합 배포용)
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/**")
-            .addResourceLocations("classpath:/static/")
-            .setCachePeriod(31536000) // 1년 캐시 (운영환경 최적화)
     }
 
     override fun addInterceptors(registry: InterceptorRegistry) {
